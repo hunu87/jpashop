db8db18f4761ed319a1d847b8f3c8ab4dba9f930
이현우 <ihyeon-u@ihyeon-uui-MacBookPro.local>
2021-02-20 18:21:29 +0900

    entity class 2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -12,4 +12,17 @@ public class Address {
 	private String street;
 	private String zipcode;
 	
+	protected Address() {
+	}
+	
+	// 값 타입 Entity는 멤버변수 값을 변경 못하도록 해야 함. 
+	// @Setter는 막아두고 생성자 호출에 의한 값 세팅만 가능하도록 해서, 외부에서 값을 변경 못하도록 한다.
+	public Address(String city, String street, String zipcode) {
+		super();
+		this.city = city;
+		this.street = street;
+		this.zipcode = zipcode;
+	}
 }
+
+	
